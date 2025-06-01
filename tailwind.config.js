2dcbd34d0557bb6c48c683d9748f03dd3c3f350d
SardorQobilov <sardorqobilov555@gmail.com>
2025-06-01 23:42:26 +0500

    S

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,8 @@ module.exports = {
       boxShadow: {
         'theme-xs': '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
         'theme-md': '0px 4px 6px -2px rgba(16, 24, 40, 0.05), 0px 12px 16px -4px rgba(16, 24, 40, 0.10)',
-        'theme-lg': '0px 8px 8px -4px rgba(16, 24, 40, 0.04), 0px 20px 24px -4px rgba(16, 24, 40, 0.10)'
+        'theme-lg': '0px 8px 8px -4px rgba(16, 24, 40, 0.04), 0px 20px 24px -4px rgba(16, 24, 40, 0.10)',
+        'input-focus': '0px 0px 0px 4px #465FFF1F'
       },
       backgroundImage: {
 
