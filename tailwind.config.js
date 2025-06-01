9abd70d33163e394c7d0a2bd7a73ba0afab3ad30
SardorQobilov <sardorqobilov555@gmail.com>
2025-06-01 17:08:48 +0500

    sidebar and header finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,45 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}'
-  ],
+module.exports = {
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Montserrat": ['Montserrat', 'sans-serif'],
+      },
+      fontSize: {
+        'theme-xs': '0.75rem',
+        'theme-sm': '0.875rem',
+      },
+      zIndex: {
+        '99999': '99999',
+      },
+      colors: {
+        'blue': '#407BFF',
+        'blue-light': "#F4F8FF",
+        black: '#141414',
+
+        border: {
+          light: '#DEE3F3', // Used for borders
+        },
+        bg: {
+          hover: '#F4F8FF', // Used for hover states
+          avatar: '#F9FBFF', // Used for avatar background
+        },
+        // Shadow utilities
+      },
+      boxShadow: {
+        'theme-xs': '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
+        'theme-md': '0px 4px 6px -2px rgba(16, 24, 40, 0.05), 0px 12px 16px -4px rgba(16, 24, 40, 0.10)',
+        'theme-lg': '0px 8px 8px -4px rgba(16, 24, 40, 0.04), 0px 20px 24px -4px rgba(16, 24, 40, 0.10)'
+      },
+      backgroundImage: {
+
+      },
+      screens: {
+
+      },
+
+    },
   },
   plugins: [],
 }
