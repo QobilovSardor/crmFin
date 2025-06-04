10bd2fb1bd29cc1e41ade3bc1568247eed550a6c
SardorQobilov <sardorqobilov555@gmail.com>
2025-06-04 22:04:01 +0500

    paymentArchive page finished

--- src/js/script.js
+++ src/js/script.js
@@ -219,7 +219,7 @@ class ApplicationState {
                     <path d="M1 9L1 19M13 9L13 19M7 1L7 19M19 1V19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>`,
         name: 'Статистика',
-        path: '/statistics'
+        path: '/statistics.html'
       },
       {
         icon: `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -246,7 +246,7 @@ class ApplicationState {
                     <path d="M6 8.5L8 10.5L12.5 6M17 19V5.8C17 4.11984 17 3.27976 16.673 2.63803C16.3854 2.07354 15.9265 1.6146 15.362 1.32698C14.7202 1 13.8802 1 12.2 1H5.8C4.11984 1 3.27976 1 2.63803 1.32698C2.07354 1.6146 1.6146 2.07354 1.32698 2.63803C1 3.27976 1 4.11984 1 5.8V19L3.75 17L6.25 19L9 17L11.75 19L14.25 17L17 19Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>`,
         name: 'Архив платежей',
-        path: '/payments-archive'
+        path: '/payments-archive.html'
       }
     ];
   }
